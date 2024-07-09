ca230c0e8b8a90580a7b906dcfacffc42452431d
David <david.om@yahoo.com>
2024-07-09 18:42:23 +0100

    Fixed an issue in the run-seed file and removed unwated dependencies in package.json

--- db/seeds/run-seed.js
+++ db/seeds/run-seed.js
@@ -1,6 +1,7 @@
-const seed = require("./seed");
-const users = require("../data/development/users");
-const cities = require("../data/development/cities");
+const seed = require('./seed');
+const users = require('../data/development/users');
+const cities = require('../data/development/cities');
+const db = require('../connection');
 
 const runSeed = () => {
   return seed(users, cities).then(() => {
